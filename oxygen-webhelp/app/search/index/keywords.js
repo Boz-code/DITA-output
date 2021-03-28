807a0032c8a520705d0a197a5f1e5b7f230e498c
Boz-code <duzynska.b@gmail.com>
2021-03-28 15:09:30 +0200

    commit all files od DITA homework

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,18 @@
+define(function() {var keywords=[{w:"About",p:["p0"]},{w:"SUP",p:["p0","p1","p2","p3","p4","p8"]},{w:"-",p:["p1"]},{w:"how",p:["p1","p9","p10"]},{w:"to",p:["p1","p9","p10"]},{w:"history",p:["p2"]},{w:"reference",p:["p3"]},{w:"Types",p:["p4"]},{w:"of",p:["p4"]},{w:"boards",p:["p4"]},{w:"Glossary",p:["p5"]},{w:"EVA",p:["p6"]},{w:"inflatable",p:["p7"]},{w:"paddle?",p:["p9"]},{w:"pump",p:["p10"]},{w:"a",p:["p10"]},{w:"paddle",p:["p10"]},{w:"board?",p:["p10"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[1, 2, 3, 4];
+ph["p2"]=[1, 5];
+ph["p3"]=[1, 6];
+ph["p4"]=[7, 8, 1, 9];
+ph["p5"]=[10];
+ph["p6"]=[11];
+ph["p7"]=[12];
+ph["p8"]=[1];
+ph["p9"]=[3, 4, 13];
+ph["p10"]=[3, 4, 14, 15, 16, 17];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,4 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+define(function () {
+return {6:5,2:0,4:0,7:5,1:-1,9:1,10:1,0:-1,3:0,5:-1,8:5};
+});
